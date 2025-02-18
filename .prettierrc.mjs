538dbc8153cfaf9dde2f8929a317c504996b283b
Gunn <gunn.connect@gmail.com>
2025-02-18 14:07:10 +0700

    initial project

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,12 +1,18 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ['prettier-plugin-astro'],
   overrides: [
     {
-      files: "*.astro",
+      files: '*.astro',
       options: {
-        parser: "astro",
+        parser: 'astro',
+        singleQuote: true,
+        semi: true,
+        tabWidth: 2,
+        printWidth: 100,
+        trailingComma: 'es5',
+        astroAllowShorthand: false
       },
     },
   ],
